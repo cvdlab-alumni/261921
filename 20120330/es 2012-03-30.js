319afd591a1deda84d2f6ff4e9ff4a504284510b
Valerio D. Di Paolo <valeriodipa@hotmail.com>
2012-04-01 17:50:55 +0200

    commit

---
+++ 20120330/es 2012-03-30.js
@@ -0,0 +1,9 @@
+var f = SIMPLICIALCOMPLEX([[0,0],[1,0],[1,1],[0,1]]);
+var quad = f([[0,1,3],[1,2,3]]);
+DRAW(quad);
+
+
+
+var quad2 = SIMPLICIALCOMPLEX([[0,0],[1,0],[1,1],[0,1]]) ([[0,1,3],[1,2,3]]);
+DRAW(quad2);
+
