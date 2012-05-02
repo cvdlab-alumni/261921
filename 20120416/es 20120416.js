e72ca07856ebc4fbfcfd2438d91ebedc46545a26
Valerio D. Di Paolo <valeriodipa@hotmail.com>
2012-05-02 14:59:40 +0200

    commit

--- 20120416/es 20120416.js
+++ 20120416/es 20120416.js
@@ -1,14 +1,16 @@
 // 2012-04-16
+// esempio pg. 15 slide 2012-04-16
 
 var link = T([0,1])([-1,-19])( CUBOID([2, 20]) );
 
 var joint = function (alpha){
-	return COMP([ T([1])([-18]), R([2])(alpha * PI/180)])
+	return COMP([ T([1])([-18]), R([0,1])([alpha * PI/180]) ])
 }
 
-var arm = function (a1,a2,a3){
-	return STRUCT([link, joint(a1), link, joint(a2), link, joint(a3), link])
+var arm = function (a1,a2,a3) {
+	return STRUCT([ link, joint(a1), link, joint(a2), link, joint(a3), link ])
 }
 
 //DRAW(arm(90,90,90));
-DRAW(arm(45,15,30));
+//DRAW(arm(45,15,30));
+DRAW(arm(60,45,30));
